dd6710566c46a1a6141bb1eedc719aacc96904dd
vegeta897 <vegeta897@gmail.com>
2015-05-31 07:02:48 -0400

    Created worker list, class, and button prefab, #6 related

---
+++ Assets/Scripts/Worker.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Worker : MonoBehaviour 
+{
+
+    public Text TxtName;
+    public Text TxtStatus;
+
+	void Start () 
+    {
+	    
+	}
+	
+	void Update () 
+    {
+	    
+	}
+}
