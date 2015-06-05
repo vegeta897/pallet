be08913aa602ce3fd3e0ab54e74a903ae57ecf80
Raziaar <raziaar@gmail.com>
2015-06-05 00:34:24 -0500

    Created InventoryManager, Rack and StorageItem objects for racking system

---
+++ Assets/Scripts/Rack.cs
@@ -0,0 +1,139 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Rack : MonoBehaviour
+{
+
+    private class Shelf
+    {
+
+        private List<StorageItem> storedItems;
+        public List<StorageItem> StoredItems
+        {
+            get { return storedItems; }
+            set { storedItems = value; }
+        }
+
+        private int shelfMaxWeight;
+        public int ShelfMaxWeight
+        {
+            get { return shelfMaxWeight; }
+            set { shelfMaxWeight = value; }
+        }
+
+        private int remainingWeight;
+        public int RemainingWeight
+        {
+            get { return remainingWeight; }
+            set { remainingWeight = value; }
+        }
+
+        private float availableWidth;
+        public float AvailableWidth
+        {
+            get { return availableWidth; }
+            set { availableWidth = value; }
+        }
+
+        private float maxWidth;
+        public float MaxWidth
+        {
+          get { return maxWidth; }
+          set { maxWidth = value; }
+        }
+
+        public Shelf(float numPalletWidths, int maxWeight)
+        {
+            ShelfMaxWeight = maxWeight;
+            maxWidth = numPalletWidths;
+        }
+        
+        public bool PutItem(StorageItem item)
+        {
+            if (AvailableWidth + item.Width <= MaxWidth &&
+                RemainingWeight + item.Weight <= ShelfMaxWeight)
+            {
+                StoredItems.Add(item);
+                AvailableWidth -= item.Width;
+                RemainingWeight -= item.Weight;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public StorageItem GetItem(StorageItem item)
+        {
+            var storedItem = StoredItems.Find(i => i == item);
+            if (storedItem != null)
+            {
+                // Found item. Remove from inventory and alter capacity.
+                StoredItems.Remove(storedItem);
+                AvailableWidth += item.Width;
+                RemainingWeight += item.Weight;
+
+                return storedItem;
+            }
+
+            // Found nothing
+            return null;
+        }
+
+        // Method to check if shelf has any free room to put a storage item.
+    }
+
+
+
+
+
+    Dictionary<int, Shelf> shelves;
+    private Dictionary<int, Shelf> Shelves
+    {
+        get { return shelves; }
+        set { shelves = value; }
+    }
+
+    public int RackMaxCapacity
+    {
+        get
+        {
+            int capacity = 0;
+            for (int i = 0; i < Shelves.Count; i++)
+            {
+                capacity += Shelves[i].ShelfMaxWeight;
+            }
+            return capacity;
+        }
+    }
+
+    public Rack(float numPalletWidths, int totalHeight, int numShelves, int maxCapacity)
+    {
+        for (int i = 1; i <= numShelves; i++)
+        {
+            Shelves.Add(i, new Shelf(numPalletWidths, numShelves / maxCapacity));
+        }
+    }
+
+    // Method to check if item exists. Delegate object may be able to be used?
+
+    // GetGoods()
+
+    // PutGoods()
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/StorageItem.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StorageItem : MonoBehaviour
+{
+    // Temporary
+    private int storedGoods;
+    public int StoredGoods
+    {
+        get { return storedGoods; }
+        set { storedGoods = value; }
+    }
+
+    // Temporary
+    private int storedGoodBaseWeight;
+    public int StoredGoodBaseWeight
+    {
+        get { return storedGoodBaseWeight; }
+        set { storedGoodBaseWeight = value; }
+    }
+
+    private int baseWeight;
+    public int BaseWeight
+    {
+        get { return baseWeight; }
+        set { baseWeight = value; }
+    }
+
+    private float width;
+    public float Width
+    {
+        get { return width; }
+        set { width = value; }
+    }
+
+    public int Weight
+    {
+        // Base weight of the storage item plus the weight for all stored items.
+        get { return BaseWeight + (StoredGoods * StoredGoodBaseWeight); }
+    }
+
+    private int maxWeight;
+    public int MaxWeight
+    {
+        get { return maxWeight; }
+        set { maxWeight = value; }
+    }
+
+    public StorageItem(float width = 1, int weight = 15, int maxWeight = 1000, int storedItemBaseWeight = 1)
+    {
+        StoredGoods = 0;
+        BaseWeight = weight;
+        MaxWeight = maxWeight;
+        Width = width;
+        StoredGoodBaseWeight = storedItemBaseWeight;
+    }
+
+    // Temporary
+    public int RemoveGoods(int quantity)
+    {
+        if (StoredGoods >= quantity)
+        {
+            StoredGoods -= quantity;
+            return quantity;
+        }
+
+        return 0;
+    }
+
+    public bool AddGoods(int quantity)
+    {
+        if (Weight + (quantity * StoredGoodBaseWeight) <= MaxWeight)
+        {
+            StoredGoods += quantity;
+            return true;
+        }
+
+        return false;
+    }
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
